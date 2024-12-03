1d99f487f96b05bdea39cf46919d2344bf9501c6
gxnie <wlsdl7520@naver.com>
2024-12-03 13:00:03 +0900

    🎉 add init commit

---
+++ src/main/java/com/example/samsung_delivery/SamsungDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.example.samsung_delivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamsungDeliveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SamsungDeliveryApplication.class, args);
+    }
+
+}
