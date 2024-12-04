a3d4865634fa5e514932ad5de8a4b73884b8c41e
sooyeon_heo <suzi97@naver.com>
2024-12-04 20:54:34 +0900

    🎉 add : Menu 생성 (Create)

--- src/main/java/com/example/samsung_delivery/config/Const.java
+++ src/main/java/com/example/samsung_delivery/config/Const.java
@@ -2,6 +2,4 @@ package com.example.samsung_delivery.config;
 
 public class Const {
     public  static final String LOGIN_USER = "loginUser";
-
-    public  static final String LOGIN_OWNER = "loginOwner";
 }
---
+++ src/main/java/com/example/samsung_delivery/controller/MenuController.java
@@ -0,0 +1,42 @@
+package com.example.samsung_delivery.controller;
+
+import com.example.samsung_delivery.config.Const;
+import com.example.samsung_delivery.dto.login.LoginResponseDto;
+import com.example.samsung_delivery.dto.menu.MenuRequestDto;
+import com.example.samsung_delivery.dto.menu.MenuResponseDto;
+import com.example.samsung_delivery.service.MenuService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/menus")
+public class MenuController {
+
+    private final MenuService menuService;
+
+    @PostMapping
+    public ResponseEntity<MenuResponseDto> createMenu(
+            @RequestBody MenuRequestDto dto,
+            HttpServletRequest request) {
+
+        HttpSession session = request.getSession(false);
+        if (session == null) {
+            throw new IllegalArgumentException("로그인이 필요합니다.");
+        }
+
+        LoginResponseDto loginUser = (LoginResponseDto) session.getAttribute(Const.LOGIN_USER);
+
+        MenuResponseDto menuResponseDto = menuService.createMenu(dto.getStoreId(), loginUser.getEmail(), dto.getMenuName(), dto.getPrice());
+        return ResponseEntity.status(HttpStatus.CREATED).body(menuResponseDto);
+    }
+
+}
+
