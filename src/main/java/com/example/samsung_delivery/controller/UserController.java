92c39dd58826dbcfe23c88d33cc4322b26560cd1
gxnie <wlsdl7520@naver.com>
2024-12-04 14:40:21 +0900

    🚚move: login 기능 User로 병합

--- src/main/java/com/example/samsung_delivery/controller/UserController.java
+++ src/main/java/com/example/samsung_delivery/controller/UserController.java
@@ -1,9 +1,14 @@
 package com.example.samsung_delivery.controller;
 
+import com.example.samsung_delivery.config.Const;
+import com.example.samsung_delivery.dto.login.LoginRequestDto;
+import com.example.samsung_delivery.dto.login.LoginResponseDto;
 import com.example.samsung_delivery.dto.user.UserRequestDto;
 import com.example.samsung_delivery.dto.user.UserResponseDto;
-import com.example.samsung_delivery.service.LoginService;
 import com.example.samsung_delivery.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,7 +21,6 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
     private final UserService userService;
-    private final LoginService loginService;
 
     // 회원가입
     @PostMapping("/signup")
@@ -24,6 +28,22 @@ public class UserController {
         UserResponseDto userResponseDto = userService.signUp(userDto.getEmail(), userDto.getPassword(), userDto.getRole());
 
         return new ResponseEntity<>(userResponseDto, HttpStatus.CREATED);
+    }
+
+    // 사용자 로그인
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDto> login(
+            @Valid @RequestBody LoginRequestDto loginRequestDto, // 로그인 요청 데이터
+            HttpServletRequest servletRequest // HTTP 요청 객체
+    ) {
+        // 로그인 서비스 호출
+        LoginResponseDto loginResponseDto = userService.login(loginRequestDto.getEmail(), loginRequestDto.getPassword());
+
+        // 세션 생성 및 사용자 정보 저장
+        HttpSession httpSession = servletRequest.getSession();
+        httpSession.setAttribute(Const.LOGIN_USER, loginResponseDto);
+
+        return new ResponseEntity<>(loginResponseDto, HttpStatus.OK); // 로그인 성공 결과 반환
     }
 
     // 회원탈퇴
@@ -35,4 +55,8 @@ public class UserController {
             userService.deactivateUser(userId, userDto.getPassword());
             return ResponseEntity.ok("회원 탈퇴되었습니다.");
     }
+
+
+
+
 }
--- src/main/java/com/example/samsung_delivery/service/UserService.java
+++ src/main/java/com/example/samsung_delivery/service/UserService.java
@@ -1,5 +1,6 @@
 package com.example.samsung_delivery.service;
 
+import com.example.samsung_delivery.dto.login.LoginResponseDto;
 import com.example.samsung_delivery.dto.user.UserResponseDto;
 import com.example.samsung_delivery.enums.UserRole;
 import com.example.samsung_delivery.enums.UserStatus;
@@ -10,5 +11,7 @@ public interface UserService {
     UserResponseDto signUp(String email, String password, UserRole role);
 
     void deactivateUser(Long userId, String password);
+
+    LoginResponseDto login(String email, String password);
 
 }
