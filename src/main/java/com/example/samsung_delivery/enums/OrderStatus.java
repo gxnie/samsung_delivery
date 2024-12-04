c4ebe20674a915295ce7aa3e6f85684b4bc99936
searchcolor <qkrdydwo1022@gmail.com>
2024-12-04 09:49:16 +0900

    add OrderStatus enum 추가

---
+++ src/main/java/com/example/samsung_delivery/enums/OrderStatus.java
@@ -0,0 +1,21 @@
+package com.example.samsung_delivery.enums;
+
+public enum OrderStatus {
+
+    //주문수락
+    ACCEPT_ODER,
+
+    //조리중
+    COOKING,
+
+    //조리완료
+    COOKING_COMPLETED,
+
+    //배송중
+    ON_DELIVERY,
+
+    //배송완료
+    DELIVERY_COMPLETED;
+
+
+}
