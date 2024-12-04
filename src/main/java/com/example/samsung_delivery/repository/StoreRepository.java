387c03e95f31437198c3c2a2cb6a5c6f2f1b4a7e
heroKim997 <ghkufud6545@gmail.com>
2024-12-04 17:20:02 +0900

    fix. 코드 리팩토링

--- src/main/java/com/example/samsung_delivery/repository/StoreRepository.java
+++ src/main/java/com/example/samsung_delivery/repository/StoreRepository.java
@@ -3,17 +3,8 @@ package com.example.samsung_delivery.repository;
 import com.example.samsung_delivery.entity.Store;
 import com.example.samsung_delivery.enums.StoreStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
-import java.util.Optional;
 
 public interface StoreRepository extends JpaRepository<Store, Long> {
-
-//    @Query("select count(*)" +
-//            "from Store st "+
-//            "where st.user.id = :userId ")
-//    Optional<Long> findTotalStoreCount(@Param("userId") String userId);
     Long countByUserIdAndStatus(Long userId, StoreStatus status);
 
 }
